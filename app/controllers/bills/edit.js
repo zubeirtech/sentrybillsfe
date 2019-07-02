1c0065875715ad113b64c0c8498720d3ab68e4bb
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-02 04:53:19 +0200

    setup edit route template

--- app/controllers/bills/edit.js
+++ app/controllers/bills/edit.js
@@ -1,4 +1,16 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    toastr: service('toast'),
+
+    actions: {
+
+        editBill() {
+            this.model.save();
+            this.transitionToRoute('/bills');
+            this.toastr.success('Successfully edited Bill', 'Nice!')
+        }
+    }
+
 });
--- app/routes/bills/edit.js
+++ app/routes/bills/edit.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    async model(params) {
+        return this.store.findRecord('bill', params.bills_id)
+    },
 });
