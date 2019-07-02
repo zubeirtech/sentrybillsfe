7daae05d14315732697b932382084ddc24fae88d
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-02 04:21:51 +0200

    Add delete function

--- app/controllers/bills/index.js
+++ app/controllers/bills/index.js
@@ -1,9 +1,22 @@
 import Controller from '@ember/controller';
 import { set } from '@ember/object';
-import nearest from 'nearest-date';
+// import nearest from 'nearest-date';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    toastr: service('toast'),
     totalNum: 0,
+    setProgressAndFinished(model) {
+        let inc = 0;
+        model.forEach(element => {
+            if(element.balanced) {
+                inc++;
+            }
+        });
+        const num = inc / model.length * 100;
+        set(this, 'percentage', num);
+        set(this, 'balanced', inc);
+    },
 
     setup(model) {
         if (model.length === 0) {
@@ -15,16 +28,9 @@ export default Controller.extend({
             set(this, 'color', 'green');
         }
         // progress
-        let inc = 0;
-        model.forEach(element => {
-            if(element.balanced) {
-                set(this, 'inc', inc+1);
-            }
-        });
-        const num = inc / model.length * 100;
-        set(this, 'percentage', num);
-        set(this, 'balanced', inc);
+        this.setProgressAndFinished(model);
 
+        //total
         let sum = 0;
         model.forEach(bill => {
             sum += bill.total;
@@ -33,13 +39,13 @@ export default Controller.extend({
         set(this, 'totalNum', sum);
 
         //next due
-        let datesArr = [];
-        model.forEach(doc => {
-            console.log
-            datesArr.pushObject(doc.due);
-        });
-        let closestIndex = nearest(datesArr, Date.now());
-        console.log(closestIndex);
+        // let datesArr = [];
+        // model.forEach(doc => {
+        //     console.log
+        //     datesArr.pushObject(doc.due);
+        // });
+        // let closestIndex = nearest(datesArr, Date.now());
+        // console.log(closestIndex);
     },
 
     actions: {
@@ -48,9 +54,20 @@ export default Controller.extend({
             try {
                 set(model, 'balanced', true);
                 model.save();
+                this.setProgressAndFinished(this.model);
+                this.toastr.success('Bill is balanced', 'Nice!')
             } catch (error) {
                 console.log(error);
             }    
+        },
+        async deleteBill(bill) {
+            try {
+                await bill.destroyRecord();
+                this.setProgressAndFinished(this.model);
+                this.toastr.success('Bill is deleted', 'Nice!')
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 });
--- app/routes/bills/new.js
+++ app/routes/bills/new.js
@@ -4,7 +4,6 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
     session: service(),
     toastr: service('toast'),
-    userHasEnteredData: true,
 
     model() {
         let userId = this.session.data.authenticated.access_token;
@@ -12,15 +11,4 @@ export default Route.extend({
             userId
         })
     },
-
-    actions: {
-        willTransition(transition) {
-          if (this.userHasEnteredData && !window.confirm('By leaving the form you are deleting changes, are you sure?')) {
-            transition.abort();
-          } else {
-            window.location.href = '/bills';
-            return true;
-          }
-        }
-    }
 });
