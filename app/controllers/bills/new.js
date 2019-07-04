7405e57fbe3a69b89a77e93d6381357dedaee39a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-04 04:22:42 +0200

    Set notification

--- app/controllers/bills/new.js
+++ app/controllers/bills/new.js
@@ -32,12 +32,50 @@ export default Controller.extend({
                     let d = new Date();
                     let currDate = d.toISOString();
                     if(currDate < this.model.due || this.model.due === undefined) {
-                        await this.model.save();
-                        this.toastr.success('Added new Bill', 'Success!');
-                        set(this, 'confirm', true);
-                        this.transitionToRoute('/bills');
+                        if (this.model.addToCal) {
+                            if (this.model.due === undefined || this.model.due === "") {
+                                this.toastr.warning('You activated notfication without a date, please add a date', 'Warning')
+                            } else {
+                                let result = await Notification.requestPermission();
+                                if (result === 'denied') {
+                                    this.toastr.info('Notification is disabled', 'Info');
+                                    await this.model.save();
+                                    this.toastr.success('Added new Bill', 'Success!');
+                                    set(this, 'confirm', true);
+                                    this.transitionToRoute('/bills');
+                                }
+                                if ( result === 'default') {
+                                    await this.model.save();
+                                    this.toastr.success('Added new Bill', 'Success!');
+                                    set(this, 'confirm', true);
+                                    this.transitionToRoute('/bills');
+                                }
+                                if (result === 'granted') {
+                                    let options = {
+                                        body: `${this.model.name} Bill is due. The total is ${this.model.total}`,
+                                        timestamp: this.model.due,
+                                        //ICON
+                                    }
+                                    
+                                    let d = Date.parse(this.model.due);
+                                    let n = Date.now();
+                                    let ms = d - n;
+                                    setTimeout(() => {
+                                        new Notification(`New Bill ${this.model.name} - Sentrybills Notification`, options);
+                                    }, ms);
+                                    await this.model.save();
+                                    this.toastr.success('Added new Bill', 'Success!');
+                                    set(this, 'confirm', true);
+                                    this.transitionToRoute('/bills');
+                                }
+                                await this.model.save();
+                                this.toastr.success('Added new Bill', 'Success!');
+                                set(this, 'confirm', true);
+                                this.transitionToRoute('/bills');
+                            }
+                        }
                     } else {
-                        this.toastr.error('Date is in the past' ,'Please use future')
+                        this.toastr.warning('Date is in the past or undefined' ,'Warning')
                     }
                 } else {
                     this.toastr.error('Please fill in correct values', 'Warning!')
