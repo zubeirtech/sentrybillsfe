34dd379e53bc10e17232dcc98642e29772a80fb2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-04 06:31:02 +0200

    add favicon and logo and font-family

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,12 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
     session: service(),
+    toastr: service('toast'),
 
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
+        this.toastr.error('Password or email is wrong', 'Try again!');
       });
     }
   }
