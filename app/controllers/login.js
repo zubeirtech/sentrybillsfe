567b3f8c7d34210af17d1a665bdcce76cdc445a8
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-04 13:15:22 +0200

    Little adjustment in edit page and loader

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,16 +1,20 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default Controller.extend({
     session: service(),
     toastr: service('toast'),
+    loader: false,
 
   actions: {
     authenticate() {
+      set(this, 'loader', true);
       let { identification, password } = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
         this.toastr.error('Password or email is wrong', 'Try again!');
+        set(this, 'loader', false)
       });
     }
   }
