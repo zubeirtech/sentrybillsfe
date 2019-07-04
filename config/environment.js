d5e9256f7366cc5820475036fb745ab6627e15c4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-04 09:27:37 +0200

    add contentsecuritypolicies

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+      'font-src': "'self'",
+      'connect-src': "'self' https://sentrybillsapi.herokuapp.com",
+      'img-src': "'self'",
+      'report-uri':"'localhost'",
+      'style-src': "'self' 'unsafe-inline'",
+      'frame-src': "'none'"
     }
   };
 
