d851485b8035767fd6c09addf45a5fc848883b37
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-14 04:54:33 +0200

    final configs to convert to pwa

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/zonkyio/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "frontend",
+    short_name: "frontend",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "https://i.imgur.com/oB9AQ7o.png",
+        sizes: "540x540",
+        type: "image/png"
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
